8d7967a452aff398d59b4ba9ab4f74ceb06a6d0b
SimonSta95 <simonsta95@gmail.com>
2024-03-21 19:50:24 +0100

    Updated Main page with new idea

--- index.js
+++ index.js
@@ -1,64 +1,93 @@
-document.querySelector('#search').addEventListener('click', getPokemon);
+const MAX_POKEMON = 151;
+const listWrapper = document.querySelector(".list-wrapper");
+const searchInput = document.querySelector("#search-input");
+const numberFilter = document.querySelector("#number");
+const nameFilter = document.querySelector("#name");
+const notFoundMessage = document.querySelector("#not-found-message");
+const closeButton = document.querySelector(".search-close-icon");
 
-function capitalizeFirstChar(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+let allPokemons = [];
+
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
+    .then((response) => response.json())
+    .then((data) => {
+        allPokemons = data.results;
+        displayPokemons(allPokemons);
+    })
+
+searchInput.addEventListener("keyup",handleSearch);
+closeButton.addEventListener("click", clearSearch);
+
+async function fetchPokemonDataBeforeRedirect(id) {
+    try {
+        const [pokemon, pokemonSpecies] = 
+        await Promise.all([
+            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => { res.json()}), 
+            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => { res.json()})
+        ]);
+        return true;
+    } catch (error) {
+        console.error("Failed to fetch Pokemon data before redirect");
+    }
 }
 
-function lowerCaseName(string) {
-    return string.toLowerCase();
-}
-
-function calcHeight(int){
-    return int.toString().slice(0, -1) + ',' + int.toString().slice(-1) + ' m';
-}
-
-function calcWeight(int){
-    return int.toString().slice(0, -1) + ',' + int.toString().slice(-1) + ' kg'
-}
-
-function getPokemon(e) {
-    const name = lowerCaseName(document.querySelector("#pokemonName").value);
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-        .then((response) => response.json())
-        .then((data) => {
-            const hpStat = data.stats.find(stat => stat.stat.name === 'hp');
-            const attackStat = data.stats.find(stat => stat.stat.name === 'attack');
-            const defenseStat = data.stats.find(stat => stat.stat.name === 'defense');
-            const spattackStat = data.stats.find(stat => stat.stat.name === 'special-attack');
-            const spdefenseStat = data.stats.find(stat => stat.stat.name === 'special-defense');
-            const speedStat = data.stats.find(stat => stat.stat.name === 'speed');
-
-            document.querySelector('.pokemonBox').innerHTML =
-            `
-            <div class="pokemonInfo">
-                <div>
-                    <img 
-                        src="${data.sprites.other["official-artwork"].front_default}" 
-                        alt="${capitalizeFirstChar(data.name)}" 
-                    />
-                </div>
-                <h1>${capitalizeFirstChar(data.name)}</h1>
-                <p><strong>Height:</strong> ${calcHeight(data.height)}</p>
-                <p><strong>Weight:</strong> ${calcWeight(data.weight)}</p>
-                <br>
-                <h2><Strong>Base Stats</Strong></h2>
-                <div class="stats-group">
-                    <p><strong>Hp:</strong> ${hpStat.base_stat}</p>
-                    <p><strong>Attack:</strong> ${attackStat.base_stat}</p>
-                    <p><strong>Defense:</strong> ${defenseStat.base_stat}</p>
-                </div>
-                <div class="stats-group">
-                    <p><strong>Special-Attack:</strong> ${spattackStat.base_stat}</p>
-                    <p><strong>Special-Defense:</strong> ${spdefenseStat.base_stat}</p>
-                    <p><strong>Speed:</strong> ${speedStat.base_stat}</p>
-                </div>
+function displayPokemons(pokemon) {
+    listWrapper.innerHTML = "";
+    pokemon.forEach((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        const listItem = document.createElement("div");
+        listItem.className = "list-item";
+        listItem.innerHTML = `
+            <div class="number-wrap">
+                <p class="caption-fonts">#${pokemonID}</p>
             </div>
-            `
-        })
-        .catch((err) => {
-            console.log("Pokemon not found", err);
-        });
+            <div class="img-wrap">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${pokemon.name}"/>
+            </div>
+            <div class="name-wrap">
+                <p class="body3-fonts">${pokemon.name}</p>
+            </div>
+        `;
 
-    e.preventDefault();
+        listItem.addEventListener("click",async () => {
+            const success = await fetchPokemonDataBeforeRedirect(pokemonID);
+            if (success) {
+                window.location.href = `./detail.html?id=${pokemonID}`;
+            }
+        })
+
+        listWrapper.appendChild(listItem);
+    });
+}
+
+function handleSearch() {
+    const searchTerm = searchInput.value.toLowerCase();
+    let filteredPokemons;
+  
+    if (numberFilter.checked) {
+      filteredPokemons = allPokemons.filter((pokemon) => {
+        const pokemonID = pokemon.url.split("/")[6];
+        return pokemonID.startsWith(searchTerm);
+      });
+    } else if (nameFilter.checked) {
+      filteredPokemons = allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().startsWith(searchTerm)
+      );
+    } else {
+      filteredPokemons = allPokemons;
+    }
+  
+    displayPokemons(filteredPokemons);
+  
+    if (filteredPokemons.length === 0) {
+      notFoundMessage.style.display = "block";
+    } else {
+      notFoundMessage.style.display = "none";
+    }
+}
+
+function clearSearch() {
+    searchInput.value = "";
+    displayPokemons(allPokemons);
+    notFoundMessage.style.display = "none";
 }

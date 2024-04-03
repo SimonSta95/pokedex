9e8f4fcad6d511e74cffb5cb984edef0d9a61365
Simon Staß <simonsta95@gmail.com>
2024-04-03 13:55:28 +0200

    Reworked Project

--- index.js
+++ index.js
@@ -1,93 +1,91 @@
-const MAX_POKEMON = 151;
-const listWrapper = document.querySelector(".list-wrapper");
-const searchInput = document.querySelector("#search-input");
-const numberFilter = document.querySelector("#number");
-const nameFilter = document.querySelector("#name");
-const notFoundMessage = document.querySelector("#not-found-message");
-const closeButton = document.querySelector(".search-close-icon");
+//Get Elements
+const pokedex    = document.querySelector('.pokedex');
+const search     = document.querySelector('.search');
+const number     = document.querySelector('.number');
+const image      = document.querySelector('.image')
+const types      = document.querySelector('.types');
+const baseStats  = document.querySelector('.base-stats');
+const statDesc   = document.querySelectorAll('.stat-desc');
+const statNumber = document.querySelectorAll('.stat-number');
+const barOuter   = document.querySelectorAll('.bar-outer');
+const barInner   = document.querySelectorAll('.bar-inner');
 
-let allPokemons = [];
+//Colors for type badges
+const typeColors = {
+    normal: "#A8A77A",
+    fire: "#EE8130",
+    water: "#6390F0",
+    electric: "#F7D02C",
+    grass: "#7AC74C",
+    ice: "#96D9D6",
+    fighting: "C22E28",
+    poison: "A33EA1",
+    ground: "E2BF65",
+    flying: "A98FF3",
+    psychic: "F95587",
+    bug: "A6B91A",
+    rock: "B6A136",
+    ghost: "735797",
+    dragon: "6F35FC",
+    dark: "705746",
+    steel: "B7B7CE",
+    fairy: "D685AD"
+};
 
-fetch(`https://pokeapi.co/api/v2/pokemon?limit=${MAX_POKEMON}`)
-    .then((response) => response.json())
-    .then((data) => {
-        allPokemons = data.results;
-        displayPokemons(allPokemons);
-    })
+const Api = async (pokemon) => {
+    pokemonApi = pokemon.split(' ').join('-').toLowerCase();
 
-searchInput.addEventListener("keyup",handleSearch);
-closeButton.addEventListener("click", clearSearch);
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemonApi);
 
-async function fetchPokemonDataBeforeRedirect(id) {
-    try {
-        const [pokemon, pokemonSpecies] = 
-        await Promise.all([
-            fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then((res) => { res.json()}), 
-            fetch(`https://pokeapi.co/api/v2/pokemon-species/${id}`).then((res) => { res.json()})
-        ]);
-        return true;
-    } catch (error) {
-        console.error("Failed to fetch Pokemon data before redirect");
+    if(response.status === 200) {
+        const data = await response.json();
+        return data;
     }
+
+    return false;
 }
 
-function displayPokemons(pokemon) {
-    listWrapper.innerHTML = "";
-    pokemon.forEach((pokemon) => {
-        const pokemonID = pokemon.url.split("/")[6];
-        const listItem = document.createElement("div");
-        listItem.className = "list-item";
-        listItem.innerHTML = `
-            <div class="number-wrap">
-                <p class="caption-fonts">#${pokemonID}</p>
-            </div>
-            <div class="img-wrap">
-                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonID}.png" alt="${pokemon.name}"/>
-            </div>
-            <div class="name-wrap">
-                <p class="body3-fonts">${pokemon.name}</p>
-            </div>
-        `;
+search.addEventListener('change', async(event) => {
+    const data = await Api(event.target.value);
 
-        listItem.addEventListener("click",async () => {
-            const success = await fetchPokemonDataBeforeRedirect(pokemonID);
-            if (success) {
-                window.location.href = `./detail.html?id=${pokemonID}`;
-            }
-        })
+    if(!data) {
+        alert('Pokémon does not exist');
+        return;
+    }
 
-        listWrapper.appendChild(listItem);
+    //Set main Backgroundcolor after first type
+    const mainColor = typeColors[data.types[0].type.name];
+    baseStats.style.color         = mainColor;
+    pokedex.style.backgroundColor = mainColor;
+
+    // Sets pokemon # at the top of the page
+    number.innerHTML = '#' + data.id.toString().padStart(3, '0');
+
+    // Sets pokemon image
+    image.src = data.sprites.other["official-artwork"].front_default;
+
+
+    // Updates "Type" bubbles
+    types.innerHTML = '';
+
+    data.types.forEach((t) => {
+        let newType = document.createElement('span');
+        let color   = typeColors[t.type.name];
+    
+        newType.innerHTML = t.type.name;
+        newType.classList.add('type');
+        newType.style.backgroundColor = color; 
+    
+        types.appendChild(newType);
     });
-}
 
-function handleSearch() {
-    const searchTerm = searchInput.value.toLowerCase();
-    let filteredPokemons;
-  
-    if (numberFilter.checked) {
-      filteredPokemons = allPokemons.filter((pokemon) => {
-        const pokemonID = pokemon.url.split("/")[6];
-        return pokemonID.startsWith(searchTerm);
-      });
-    } else if (nameFilter.checked) {
-      filteredPokemons = allPokemons.filter((pokemon) =>
-        pokemon.name.toLowerCase().startsWith(searchTerm)
-      );
-    } else {
-      filteredPokemons = allPokemons;
-    }
-  
-    displayPokemons(filteredPokemons);
-  
-    if (filteredPokemons.length === 0) {
-      notFoundMessage.style.display = "block";
-    } else {
-      notFoundMessage.style.display = "none";
-    }
-}
-
-function clearSearch() {
-    searchInput.value = "";
-    displayPokemons(allPokemons);
-    notFoundMessage.style.display = "none";
-}
+    // Update Bars
+    data.stats.forEach((s, i) => {
+        statNumber[i].innerHTML = s.base_stat.toString().padStart(3, '0');
+        barInner[i].style.width = `${s.base_stat}%`;
+        barInner[i].style.backgroundColor = mainColor;
+        barOuter[i].style.backgroundColor = mainColor;
+        statDesc[i].style.color           = mainColor;
+    });
+    
+});
